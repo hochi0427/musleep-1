6f09b315dc34d8bb105166d6bdaf2099d3aec9aa
hochi <hitomiamanda@gmail.com>
2020-12-13 21:45:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/Musleep/Loading.java
@@ -0,0 +1,31 @@
+package com.example.Musleep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Loading extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        getSupportActionBar().hide();
+
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/Musleep/homescreen.java
@@ -0,0 +1,75 @@
+package com.example.Musleep;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class homescreen extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView name;
+    Button signOut;
+
+    GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        imageView = findViewById(R.id.imageView);
+        name = findViewById(R.id.name);
+        signOut = findViewById(R.id.button_sign_out);
+
+        signOut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.button_sign_out:
+                    signOut();
+                    break;
+                }
+            }
+        });
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if(acct != null) {
+            String personName = acct.getDisplayName();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            name.setText(personName);
+            Glide.with(this).load(String.valueOf(personPhoto)).into(imageView);
+        }
+    }
+    private void signOut(){
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>(){
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(homescreen.this,"Signed Out Successfully!", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
+    }
+}
