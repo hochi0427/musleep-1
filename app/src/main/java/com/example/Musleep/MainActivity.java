424bc05f78c7fdccde488fe15e2b05494fb1b1e5
hochi <hitomiamanda@gmail.com>
2021-02-26 21:52:58 +0800

    testlogin

--- app/src/main/java/com/example/Musleep/MainActivity.java
+++ app/src/main/java/com/example/Musleep/MainActivity.java
@@ -22,8 +22,14 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.OAuthCredential;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE1 = 10005;
@@ -33,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
     GoogleSignInOptions gso;
     GoogleSignInClient signInClient;
     FirebaseAuth firebaseAuth;
+    FirebaseFirestore db;
+    DocumentReference uidref;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
         signIn = findViewById(R.id.sign_in_button);
 
         firebaseAuth = firebaseAuth.getInstance();
-
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
@@ -50,7 +58,11 @@ public class MainActivity extends AppCompatActivity {
 
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
         if(signInAccount != null || firebaseAuth.getCurrentUser() != null) {
-            startActivity(new Intent(this,homescreen.class));
+            startActivity(new Intent(this,sleep_record.class));
+            Toast.makeText(getApplicationContext(), "Fuck!", Toast.LENGTH_SHORT).show();
+
+        }else{
+            Toast.makeText(getApplicationContext(), "剛進來判斷狀態", Toast.LENGTH_SHORT).show();
         }
 
         signIn.setOnClickListener(new View.OnClickListener(){
@@ -67,19 +79,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
         if (requestCode == GOOGLE_SIGN_IN_CODE){
             Task<GoogleSignInAccount> signInTask = GoogleSignIn.getSignedInAccountFromIntent(data);
 
-
         try {
+//              db = FirebaseFirestore.getInstance();
+//              uidref = db.collection("User").document(firebaseAuth.getUid());
+//              uidref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
             GoogleSignInAccount signInAcc = signInTask.getResult(ApiException.class);
             AuthCredential authCredential = GoogleAuthProvider.getCredential(signInAcc.getIdToken(),null);
             firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    Toast.makeText(getApplicationContext(),"Your Google Account is Connected to Our Application.", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(getApplicationContext(),userdata.class));
+                    Toast.makeText(getApplicationContext(), "Your Google Account is Connected to Our Application.", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), userdata.class));
+//                    if (task.isSuccessful()){
+//                        DocumentSnapshot document = task.getResult();
+//                        if(document.exists()) {
+//                            startActivity(new Intent(getApplicationContext(),homescreen.class));
+//                        }else {
+//                            Toast.makeText(getApplicationContext(), "Your Google Account is Connected to Our Application.", Toast.LENGTH_SHORT).show();
+//                            startActivity(new Intent(getApplicationContext(), sleep_record.class));
+//                        }
+//                    }else{
+//                        Log.i("GG", "GG");
+//                    }
+
+
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
